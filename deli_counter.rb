dbcbf951995fd3bbbfe1e9491a8295b5026be417
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 16:41:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(current_line)
   if current_line.size > 0
-    puts "The line is currently #{current_line}"
+    new_str = "The line is currently"
+    while 
   else puts "The line is currently empty."
   end
 end
