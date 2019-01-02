e433836b95b21f59f788fc7fffefab0cb5e7f80d
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 16:46:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(current_line)
   if current_line.size > 0
     x = 0
      current_line.each do |smtn|
-      puts "#{x}. #{current_line}"
+      puts "#{x}. #{current_line[x]}"
       x += 1
     end
   else puts "The line is currently empty."
