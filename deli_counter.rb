5ddcfa3fdc2321045a11f4a682dc484406675357
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 16:49:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(current_line)
   if current_line.size > 0
     x = 0
-    new_str = "The line is currently"
+    new_str = "The line is currently "
      while x < current_line.size
-       new_str += ("#{x}. #{current_line[x]}")
+       new_str += ("#{x + 1}. #{current_line[x]}")
        x += 1
     end
     puts new_str
