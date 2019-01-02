8eeeaf1457b200d5a479ba384818097fd813718d
Zev Spilman <zevspilman@yahoo.com>
2019-01-02 17:11:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(current_line, name)
   current_line.push(name)
-  puts "Hello, #{name}, you are number #{current_line.length +1} on line"
+  puts "Hello, #{name}, you are number #{current_line.length} on line"
 end
